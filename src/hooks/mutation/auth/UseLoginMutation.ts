3a73c13ab48bf8cb510e60bca7cfede3c6e9952e
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2025-05-03 12:02:01 +0330

    feat: Update useLoginMutation to store user role in cookies and navigate based on role

--- src/hooks/mutation/auth/UseLoginMutation.ts
+++ src/hooks/mutation/auth/UseLoginMutation.ts
@@ -1,22 +1,22 @@
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { login } from "../../../services/auth/login";  
+import { login } from "../../../services/auth/login";
 import axios from "axios";
 import Swal from "sweetalert2";
 
 const useLoginMutation = () => {
-  const [cookies, setCookies] = useCookies(["accessToken", "refreshToken","name","phone","full_name"]);
+  const [cookies, setCookies] = useCookies(["accessToken", "refreshToken", "name", "phone", "full_name", "role"]);
   const navigate = useNavigate();
 
   return useMutation<
-    any, 
-    any, 
-    { phone: string; password: string }, 
+    any,
+    any,
+    { phone: string; password: string },
     void
   >(async (data) => await login(data), {
     onSuccess: async (data) => {
-      const { AccessToken: accessToken, refreshToken } = data; 
+      const { AccessToken: accessToken, refreshToken, role } = data;
       if (!accessToken || !refreshToken) {
         Swal.fire("خطا", "توکن دریافتی نامعتبر است", "error");
         return;
@@ -26,8 +26,15 @@ const useLoginMutation = () => {
       setCookies("name", data.name, { path: "/" });
       setCookies("phone", data.phone, { path: "/" });
       setCookies("full_name", data.name, { path: "/" });
+      setCookies("role", role, { path: "/" });
       axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-      navigate("/");
+      if (role === "admin") {
+        navigate("/panel-admin/dashboard/estate-management");
+      } else if (role === "user") {
+        navigate("/dashboard");
+      } else {
+        navigate("/");
+      }
     },
     onError: (error) => {
       Swal.fire("خطا", error?.response?.data?.message || "خطایی رخ داده است", "error");
